c007dc396c131e9708032f54289752485928a3e3
Prince <princemurage@gmail.com>
2026-04-09 11:30:25 +0300

    An android designed app with integrated ads from Google admob effiecient for beginner android programmers

--- app/src/main/java/com/prince/myapplication/MainActivity.kt
+++ app/src/main/java/com/prince/myapplication/MainActivity.kt
@@ -1,18 +1,46 @@
 package com.prince.myapplication
 
 import android.content.Intent
+//import android.media.tv.AdRequest
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+
+
+import  com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import  com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+
 
 class MainActivity : AppCompatActivity() {
+
+//    The variable is only accessible inside this class
+//    A variable to store the ad once it loads
+
+    private var mInterstitialAd: InterstitialAd?= null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this)
+        val adView=findViewById<AdView>(R.id.adView)
+        val adRequest= AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+
+//        loads the interstitial ad
+        loadInterstitialAd()
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -62,6 +90,9 @@ class MainActivity : AppCompatActivity() {
 
             val hydrationIntent=Intent(applicationContext, HydrationAlert::class.java )
         startActivity(hydrationIntent)
+
+//            displays the ad
+            showInterstitialAd()
         }
 
 //        start exercise
@@ -102,5 +133,31 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+    fun loadInterstitialAd() {
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(
+            this,
+            "ca-app-pub-3940256099942544/1033173712", // Test ID
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    mInterstitialAd = ad
+                }
+
+                override fun onAdFailedToLoad(error: LoadAdError) {
+                    mInterstitialAd = null
+                }
+            }
+        )
+    }
+    //Show Interstitial ad
+    fun showInterstitialAd() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        }
+    }
 
 }
